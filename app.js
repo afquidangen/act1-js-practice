28a8c3f09ca8032b93534e0cc74cb7925e59ab90
afquidangen <alyssafayequidangen@gmail.com>
2024-09-07 00:15:05 +0800

    Completed Variables section

---
+++ app.js
@@ -0,0 +1,72 @@
+//TASK 2 SAMPLE CODE
+console.log("Hello, World!");
+let name = "Alyssa Faye Quidangen";
+console.log("My name is " + name);
+//output: Hello, World! My name is Alyssa Faye Quidangen
+
+//TASK 4
+
+//VARIABLES
+let section = ("BSCS 2A"); 
+console.log("My section is " + section);
+//output: My section is BSCS 2A
+
+const food = ("takoyaki");
+console.log("My favorite food is " + food)
+//output: My favorite food is takoyaki
+
+let hobbies = ["reading","writing", "listening to music"]
+console.log("My hobbies: " + hobbies)
+//output: My hobbies: reading,writing,listening to music
+
+//NUMBERS
+
+//Addition
+let num1 = 143;
+let num2 = 121;
+let sum = num1 + num2;
+console.log("The sum of " + num1 + " and " + num2 + " is equal to " + sum);
+//output: The sum of 143 and 121 is equal to 264
+
+//Subtraction
+const Num1 = 500;
+const Num2 = 100;
+const difference = Num1 - Num2;
+console.log("The difference of " + Num1 + " and " + Num2 + " is equal to " + difference);
+//output: The difference of 500 and 100 is equal to 400
+
+//Multiplication
+const Number1 = 12;
+const Number2 = 25;
+const product = Number1 * Number2;
+console.log("The product of " + Number1 + " and " + Number2 + " is equal to " + product);
+//output: The product of 12 and 25 is equal to 300
+
+//Division
+const favNumber1 = 10000;
+const favNumber2 = 2;
+const quotient = favNumber1 / favNumber2;
+console.log("The quotient of " + favNumber1 + " and " + favNumber2 + " is equal to " + quotient);
+// output: The quotient of 10000 and 2 is equal to 5000
+
+//BOOLEANS
+let a = 5;
+let b = 10;
+let isLess = a > b;
+let isGreater = a < b;
+let isEqual = a == b;
+let isNotEqual = a != b;
+console.log(isLess); // output: false
+console.log(isGreater); // output: true
+console.log(isEqual); //output: false
+console.log(isNotEqual); //output: true
+
+let isPresent = true;
+let isAbsent = false;
+console.log(isPresent); //output: true
+console.log(isAbsent); //output: false
+
+//OPERATORS
+
+
+
