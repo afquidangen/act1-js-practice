d74d85ee6f11da71f8bdaf4438f3105de4a84a56
afquidangen <alyssafayequidangen@gmail.com>
2024-09-07 14:24:09 +0800

    Completed Variables section

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ console.log("My name is " + name);
 //TASK 4
 
 //VARIABLES
+
+let age = 18;
+console.log("I am " + age + " years old");
+// output: I am 18 years old
+
+let course = ("Bachelor of Science in Computer Science");
+console.log("My course is " + course);
+//output: My course is Bachelor of Science in Computer Science
+
 let section = ("BSCS 2A"); 
 console.log("My section is " + section);
 //output: My section is BSCS 2A
@@ -56,9 +65,17 @@ let modulus = randNumber1 % randNumber2;
 console.log("The remainder of " + randNumber1 + " and " + randNumber2 + " is " + modulus);
 //output: The remainder of 125 and 4 is 1
 
+let anNumber1 = 10;
+let anNumber2 = 2;
+let exponentiation = anNumber1 ** anNumber2;
+console.log(anNumber1 + " raised to the power of " + anNumber2 + " is equal to " + exponentiation);
+//output: 10 raised to the power of 2 is equal to 100
+
 //BOOLEANS
+
 let a = 5;
 let b = 10;
+
 let isLess = a > b;
 let isGreater = a < b;
 let isEqual = a == b;
@@ -73,7 +90,17 @@ let isAbsent = false;
 console.log(isPresent); //output: true
 console.log(isAbsent); //output: false
 
+let j;
+console.log(Boolean(j));
+//output: false //undefined
+
+let isComplete = 10 > 9;
+let isIncomplete = 20 < 10;
+console.log(Boolean(isComplete)); //output: true
+console.log(Boolean(isIncomplete)); // output: false
+
 //COMPARISON
+
 let c = 20;
 let d = 19;
 let e = 30;
